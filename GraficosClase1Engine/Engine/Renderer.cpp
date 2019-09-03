bd953e46cd134a5658d11451d625b9ecc6f8106e
Ariel Plomer <asplomer@gmail.com>
2019-09-03 13:33:58 -0300

    añadido los shaders.txt
    hay que compilar los shaders, ver mas tutoriales

--- GraficosClase1Engine/Engine/Renderer.cpp
+++ GraficosClase1Engine/Engine/Renderer.cpp
@@ -21,7 +21,7 @@ Renderer::Renderer()
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-
+	//glsl ver
 }
 
 void Renderer::Draw() 
